592cc3c881ffd20ca06fe287eb9bf43687bb48e7
Paramjitsaikia001 <paramjitsaikia@gmail.com>
2024-03-29 20:55:10 +0530

    update

---
+++ javascript/MDN docu/ternary.js
@@ -0,0 +1,5 @@
+/*ternary operator format
+    condition?exprIfTrue:exprfFalse */  
+    const userInput = prompt("Please enter your age:");
+const result= userInput>18?"you can drive":"you cannot";
+console.log(result);
